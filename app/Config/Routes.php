7746494617c278c4c63c5d9e7a70b0855112bbd2
Afonso <67978137+AfonsoPaula@users.noreply.github.com>
2023-10-18 00:04:23 +0100

    AP: commit 4 - login interface

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Main::index');
+
+// login/logout
+$routes->get('login', 'Main::login'); // 'get' do login que há de ir para o main e para o login
+$routes->post('login_submit', 'Main::login_submit');
+$routes->get('logout', 'Main::logout');
---
+++ app/Views/login_frm.php
@@ -0,0 +1,33 @@
+<?= $this->extend('layouts/main_layout') ?>
+
+<?= $this->section('content') ?>
+
+<section class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-4 col-sm-6 col-8 p-5 text-center bg-light">
+
+            <!-- for apply this function: BaseController.php > add $helpers = ['form']; -->
+            <?= form_open('main/login_submit') ?>
+
+                <h3 class="text-center mb-3 text-black"><strong><?= APP_NAME ?></strong></h3>
+                <div class="mb-3 text-start">
+                    <label for="username" class="form-label text-black">Username</label>
+                    <input type="text" name="username" id="username" class="form-control form-control-sm" placeholder="Username" autofocus required>
+                </div>
+
+                <div class="mb-3 text-start">
+                    <label for="password" class="form-label text-black">Password</label>
+                    <input type="password" name="password" id="password" class="form-control form-control-sm" placeholder="Password" required>
+                </div>
+
+                <div class="mb-1 mt-4">
+                    <button type="submit" class="btn btn-dark w-100">Login</button>
+                </div>
+
+            <?= form_close() ?>
+
+        </div>
+    </div>
+</section>
+
+<?= $this->endSection() ?>
